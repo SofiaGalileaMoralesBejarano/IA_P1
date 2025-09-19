55b8144495d6a6139a7659453c42724d624faa6d
SofiaGalileaMoralesBejarano <sofiaxan27@gmail.com>
2025-09-19 14:51:24 -0600

    P3_Operadores Básicos.py

---
+++ P3_Operadores Básicos.py
@@ -0,0 +1,28 @@
+# Practica 3: Operadores en Python
+# Sofia Galilea Morales Bejarano 6°E
+
+# --- Datos personales pARA PRACTICAER ---
+edad = 21                 # Literal entero que representa mi edad
+peso = 66.0               # Literal flotante que representa mi peso en kilogramos
+altura = 1.71             # Literal flotante que representa mi altura en metros
+
+# --- Operadores básicos ---
+
+# Suma
+suma = edad + 5           # Sumo 5 a mi edad
+print("Edad + 5 =", suma) # Imprimo el resultado de la suma
+
+# Resta
+resta = peso - 10          # Resto 10 a mi peso
+print("Peso - 10 =", resta) # Imprimo el resultado de la resta
+
+# Multiplicación
+multiplicacion = altura * 2   # Multiplico mi altura por 2
+print("Altura * 2 =", multiplicacion) # Imprimo el resultado
+
+# División
+division = peso / altura      # Divido mi peso entre mi altura
+print("Peso / Altura =", division) # Imprimo el resultado
+
+# División entera
+division_entera = peso // altura   # Divisi_
